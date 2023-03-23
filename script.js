62103536a6052af777a9c548329386fed79734fe
Arnab-1473 <arban.nilr.1473@gmail.com>
2023-03-23 17:56:46 +0530

    updated

---
+++ script.js
@@ -0,0 +1,11 @@
+const players = ['John', 'Paul', 'George', 'Ringo'];
+
+const person = {
+  name: 'Bob',
+  age: 30,
+  city: 'New York'
+};
+
+const team = players;
+const team1 = [...players];
+const cap1 = { ...person };
